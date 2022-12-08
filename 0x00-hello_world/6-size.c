aeabf22aa26e3c0bb02f4cfe8d16ac77ae6d7aec
AddisuAman <addisuaman11@gmail.com>
2022-12-08 05:38:05 -0800

    Task 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -8,17 +8,11 @@
 
 int main(void)
 {
-	char acharacter;
-	int ainteger;
-	long along;
-	long long alonglong;
-	float afloat;
-
-	printf("size of a char: %lu byte(s)\n", sizeof(acharacter));
-	printf("size of an int: %lu byte(s)\n", sizeof(ainteger));
-	printf("size of a long int: %lu byte(s)\n", sizeof(along));
-	printf("size of a long long int: %lu byte(s)\n", sizeof(alonglong));
-	printf("size of a float: %lu byte(s)\n", sizeof(afloat));
+	printf("size of a char: %lu byte(s)\n", sizeof(char));
+	printf("size of an int: %lu byte(s)\n", sizeof(int));
+	printf("size of a long int: %lu byte(s)\n", sizeof(long int));
+	printf("size of a long long int: %lu byte(s)\n", sizeof(long long int));
+	printf("size of a float: %lu byte(s)\n", sizeof(float));
 
 	return (0);
 }
