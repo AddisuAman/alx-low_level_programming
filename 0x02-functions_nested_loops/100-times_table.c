618782ea1ec87a1e3ce6606b15a1c56edac3d854
AddisuAman <addisuaman11@gmail.com>
2022-12-14 14:59:02 -0800

    fix

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -11,7 +11,6 @@ void print_times_table(int n)
 	if (n >= 0 && n <= 15)
 	{
 		for (i = 0; i <= n; i++)
-			_putchar(' ')
 		{
 			for (j = 0; j <= n; j++)
 			{
@@ -24,11 +23,13 @@ void print_times_table(int n)
 				{
 					_putchar(',');
 					_putchar(' ');
+					_putchar(' ');
 					_putchar(k + '0');
 				}
 				else if (k >= 10 && k < 100)
 				{
 					_putchar(',');
+					_putchar(' ');
 					_putchar(' ');
 					_putchar((k / 10) + '0');
 					_putchar((k % 10) + '0');
