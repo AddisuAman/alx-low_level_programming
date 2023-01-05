a00378c6b8803dee5c260d847ee8ef31c98da92b
AddisuAman <addisuaman11@gmail.com>
2023-01-05 12:13:39 -0800

    task 6

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -13,8 +13,8 @@ int is_prime_number(int n);
  * @num: The number to be checked
  * @div: The divisor
  *
- * Return: If the number is divisible - 0
- * If the number is not divisible - 1
+ * Return: if the number is divisible - 0
+ *         if the number is not divisible - 1
  */
 int is_divisible(int num, int div)
 {
@@ -25,4 +25,24 @@ int is_divisible(int num, int div)
 		return (1);
 
 	return (is_divisible(num, div + 1));
+}
+
+/**
+ * is_prime_number - checks if a number is prime
+ * @n: the number to be checked
+ *
+ * Return: if the integer is not prime - 0
+ * if the number is prime - 1
+ */
+int is_prime_number(int n)
+{
+	int div = 2;
+
+	if (n <= 1)
+		return (0);
+
+	if (n >= 2 && n <= 3)
+		return (1);
+
+	return (is_divisible(n, div));
 }
