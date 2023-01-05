4b01d9af7467d5ab42cba03d0f5f2bd5c3104daf
AddisuAman <addisuaman11@gmail.com>
2023-01-05 12:35:21 -0800

    task 7

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -26,3 +26,41 @@ int find_strlen(char *s)
 
 	return (len);
 }
+
+/**
+ * check_palindrome - checks if a string is a palinrome
+ * @s: the string to be checked
+ * @len: the length of s
+ * @index: the index of the string to be checked
+ *
+ * Return: if the string is a palindrome - 1
+ * if the string is not a palindrome - 0
+ */
+int check_palindrome(char *s, int len, int index)
+{
+	if (s[index] == s[len / 2])
+		return (1);
+
+	if (s[index] == s[len - index - 1])
+		return (check_palindrome(s, len, index + 1));
+
+	return (0);
+}
+
+/**
+ * is_palindrome - checks if a string is a palindrome
+ * @s: the string to be checked
+ *
+ * Return: if the string is a palindrome - 1
+ * if the string is not a palindrome - 0
+ */
+int is_palindrome(char *s)
+{
+	int index = 0;
+	int len = find_strlen(s);
+
+	if (!(*s))
+		return (1);
+
+	return (check_palindrome(s, len, index));
+}
