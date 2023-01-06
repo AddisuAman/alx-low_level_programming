53d3eab9030389a7a115fdcbc931ef741da6bc8d
AddisuAman <addisuaman11@gmail.com>
2023-01-06 10:26:08 -0800

    task 2

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -1,19 +1,13 @@
 #include <stdio.h>
-#include "main.h"
-
 /**
- * main - Prints all arguments passed into main
- * @argc: Number of command line arguments
- * @argv: Array name
+ * main - a program that prints all arguments it receives.
+ * @argc: arguement count
+ * @argv: array of pointers to arguement strings
  * Return: 0
- */
-
+ **/
 int main(int argc, char *argv[])
 {
-	int i;
-
-	for (i = 0; i < argc; i++)
-	printf("%s\n", argv[i]);
-
+	while (argc--)
+		printf("%s\n", *argv++);
 	return (0);
 }
